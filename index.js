3119f545fac3953265a3d0d2010f8e7fa98a94f8
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-01 15:40:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+  let books = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  return books;
 }
 
 function renderBooks(json) {
