6b45cc979f29a5cf4a5bf3cf1e66ccefeccab1e8
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-01 15:26:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
 fetch('http://api.open-notify.org/astros.json')
 .then(function(response) {
   return response.json();
-}).then(function(renderBooks(json);
+}).then(function(renderBooks(json)
 )}
 
 function renderBooks(json) {
