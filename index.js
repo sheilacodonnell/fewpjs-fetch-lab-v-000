43e85386fe5bd0ca502acab3b4be7e7338438f93
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-01 15:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(renderBooks(json));
 }
 
 function renderBooks(json) {
